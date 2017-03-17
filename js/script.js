f4aff736328747245164d6bc7c9125a753fdcacd
Manuel Carmona <manu.carmona90@gmail.com>
2017-03-17 22:23:51 +0100

    fixed url to work on mcarmonaa.github.io/X-NAV-JQ-Ajax

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   'use strict';
 
   $.ajax({
-      url: '/embedded.html',
+      url: 'https://mcarmonaa.github.io/X-NAV-JQ-Ajax/embedded.html',
+      // url: '/embedded.html',
     })
     .done(function(data) {
       $('#myDiv').html(data);
