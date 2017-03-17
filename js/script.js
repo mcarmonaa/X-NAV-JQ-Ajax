4aaec9ce8a6588a4ecc6227e40dbc413bb8b5d4b
Manuel Carmona <manu.carmona90@gmail.com>
2017-03-17 22:34:16 +0100

    Added button to request for header

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   'use strict';
 
-  $.ajax({
-      url: 'https://mcarmonaa.github.io/X-NAV-JQ-Ajax/embedded.html',
-      // url: '/embedded.html',
-    })
-    .done(function(data) {
-      $('#myDiv').html(data);
-    })
-    .fail(function() {
-      console.log('error');
-    });
+  $('#myInput').click(function(event) {
+    $.ajax({
+        // url: 'https://mcarmonaa.github.io/X-NAV-JQ-Ajax/embedded.html',
+        url: '/embedded.html',
+      })
+      .done(function(data) {
+        $('#myDiv').html(data);
+      })
+      .fail(function() {
+        console.log('error');
+      });
 
+    event.preventDefault(event);
+  });
 });
